82566547de8f843977e342459fac5e974c176c5c
jherve <julien.jev.herve@gmail.com>
2024-03-14 18:50:27 +0100

    Make a separate folder for medias

--- src/de_quoi_parle_le_monde/medias/__init__.py
+++ src/de_quoi_parle_le_monde/medias/__init__.py
@@ -1,7 +1,7 @@
 from de_quoi_parle_le_monde.article import ArchiveCollection
 
-from de_quoi_parle_le_monde.france_tv_info import FranceTvInfoMainPage
-from de_quoi_parle_le_monde.le_monde import LeMondeMainPage
+from .france_tv_info import FranceTvInfoMainPage
+from .le_monde import LeMondeMainPage
 
 
 media_collection = {
