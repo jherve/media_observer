eab1fa8adab5e8e30436806d158c9ac8ff8c815d
jherve <julien.jev.herve@gmail.com>
2024-04-14 17:56:09 +0200

    Add an admin page

--- src/de_quoi_parle_le_monde/web.py
+++ src/de_quoi_parle_le_monde/web.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+from de_quoi_parle_le_monde.medias import media_collection
 app = FastAPI()
 
 
@@ -14,3 +15,11 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse(request=request, name="index.html", context={})
+
+
+
+@app.get("/admin", response_class=HTMLResponse)
+async def admin_index(request: Request):
+    return templates.TemplateResponse(request=request, name="admin/index.html", context={
+        "collections": media_collection
+    })
