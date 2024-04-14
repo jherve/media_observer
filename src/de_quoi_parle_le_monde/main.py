fc085585c7787542586ca114a1ef13fb76336e0b
jherve <julien.jev.herve@gmail.com>
2024-04-14 17:32:15 +0200

    Add hello world web server

--- src/de_quoi_parle_le_monde/main.py
+++ src/de_quoi_parle_le_monde/main.py
@@ -1,16 +1,20 @@
 import asyncio
+from loguru import logger
+from hypercorn.config import Config
+from hypercorn.asyncio import serve
 
+
+from de_quoi_parle_le_monde.web import app
 from de_quoi_parle_le_monde.http import HttpClient
 from de_quoi_parle_le_monde.storage import Storage
-from de_quoi_parle_le_monde.snapshot_worker import download_all, compute_embeddings
 
 
 async def main():
     http_client = HttpClient()
     storage = await Storage.create()
 
-    # await download_all(http_client, storage, 10, [18])
-    await compute_embeddings(storage)
+    await serve(app, Config())
+    logger.info("Will quit now..")
 
 
 asyncio.run(main())
