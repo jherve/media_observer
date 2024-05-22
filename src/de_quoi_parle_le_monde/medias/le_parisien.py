ccbf0f92b674560035d818a8d757a55f2049300d
jherve <julien.jev.herve@gmail.com>
2024-05-22 22:00:17 +0200

    Add le Parisien

---
+++ src/de_quoi_parle_le_monde/medias/le_parisien.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+
+from de_quoi_parle_le_monde.article import (
+    TopArticle,
+    MainArticle,
+    MainPage,
+)
+
+
+class LeParisienMainPage(MainPage):
+    @staticmethod
+    def get_top_articles(soup: BeautifulSoup):
+        all_articles = soup.select("a[data-block-name='Les_plus_lus']")
+
+        return [
+            TopArticle.create(
+                title=a.text.strip(),
+                url=a["href"],
+                rank=idx + 1,
+            )
+            for idx, a in enumerate(all_articles)
+        ]
+
+    @staticmethod
+    def get_main_article(soup):
+        main = soup.select(".homepage__top article")[0]
+        url = main.select("a")[0]
+
+        return MainArticle.create(
+            title=url.text.strip(),
+            url=url["href"],
+        )
